1df6060cd91c87d0bc170b0e9da5833466b13485
Cory Franks <franksco39@gmail.com>
2016-07-26 11:26:14 -0700

    button correctly navigates to our Pokemon Page

--- app/src/main/java/com/epicodus/pokestats/MainActivity.java
+++ app/src/main/java/com/epicodus/pokestats/MainActivity.java
@@ -1,13 +1,28 @@
 package com.epicodus.pokestats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mPokemonListButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPokemonListButton = (Button) findViewById(R.id.pokemonListButton);
+        mPokemonListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PokemonListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
