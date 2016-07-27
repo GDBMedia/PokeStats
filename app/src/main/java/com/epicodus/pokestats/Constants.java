5bbb2365e4262ee32ebc442e034396f5ab8fadc3
Garrett Biernat <laker77@gmail.com>
2016-07-27 08:37:11 -0700

    changed url

--- app/src/main/java/com/epicodus/pokestats/Constants.java
+++ app/src/main/java/com/epicodus/pokestats/Constants.java
@@ -8,8 +8,8 @@ import javax.crypto.SecretKey;
 public class Constants {
     public static final String PASSKEY = BuildConfig.PASSKEY;
     public static final String EMAILKEY = BuildConfig.EMAILKEY;
-    public static final String GETUSER_URL = "http://pokestats.ngrok.io/getuserinfo/";
-    public static final String GETPOKEMON_URL = "http://pokestats.ngrok.io/getpokemon/";
+    public static final String GETUSER_URL = "http://gdbmedia.ngrok.io/getuserinfo/";
+    public static final String GETPOKEMON_URL = "http://gdbmedia.ngrok.io/getpokemon/";
     public static final String USERNAME_PARAM = "u";
     public static final String PASSWORD_PARAM = "p";
     public static final String LOCATION_PARAM = "l";
--- app/src/main/java/com/epicodus/pokestats/ui/PokemonGradingPageActivity.java
+++ app/src/main/java/com/epicodus/pokestats/ui/PokemonGradingPageActivity.java
@@ -2,16 +2,37 @@ package com.epicodus.pokestats.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.epicodus.pokestats.R;
+import com.epicodus.pokestats.models.Pokemon;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class PokemonGradingPageActivity extends AppCompatActivity {
+    public final String TAG = this.getClass().getSimpleName();
+
+
+    @Bind(R.id.name) TextView pokename;
+    private ArrayList<Pokemon> mPokemonArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemon_grading_page);
+        ButterKnife.bind(this);
+        if (getIntent().getExtras() != null) {
+            mPokemonArrayList = getIntent().getExtras().getParcelable("pokemons");
+            Log.d(TAG, "onCreate: "+ mPokemonArrayList.getClass());
+            Log.d(TAG, "onCreate: "+mPokemonArrayList.get(Integer.parseInt(getIntent().getStringExtra("position"))));
+
+        }
+
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
