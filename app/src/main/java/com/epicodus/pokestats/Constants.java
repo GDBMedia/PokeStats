a8d35d5c09693a10f0468001af1dcc98b51fa16e
Garrett Biernat <garrett@gdbmedia.net>
2016-07-29 18:40:31 -0700

    added heroku support

--- app/src/main/java/com/epicodus/pokestats/Constants.java
+++ app/src/main/java/com/epicodus/pokestats/Constants.java
@@ -12,8 +12,8 @@ import javax.crypto.SecretKey;
 public class Constants {
     public static final String PASSKEY = BuildConfig.PASSKEY;
     public static final String EMAILKEY = BuildConfig.EMAILKEY;
-    public static final String GETUSER_URL = "http://gdbmedia.ngrok.io/getuserinfo/";
-    public static final String GETPOKEMON_URL = "http://gdbmedia.ngrok.io/getpokemon/";
+    public static final String GETUSER_URL = "https://shielded-citadel-24301.herokuapp.com/getuserinfo/";
+    public static final String GETPOKEMON_URL = "https://shielded-citadel-24301.herokuapp.com/getpokemon/";
     public static final String USERNAME_PARAM = "u";
     public static final String PASSWORD_PARAM = "p";
     public static final String LOCATION_PARAM = "l";
